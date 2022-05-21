45811f442b508f0704b785dbded34bdc9c7c0d33
Tony Chan <Tony.Chan@Shimda.com.tw>
2022-05-21 11:34:04 +0800

    he
    Signed-off-by: Tony Chan <Tony.Chan@Shimda.com.tw>

--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!2"); 
+Console.WriteLine("Hello, World!2");
+
+
+Console.WriteLine("育人吃早餐不切分支又不拉資料 搞事情看看");
